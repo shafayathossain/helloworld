c7a5568abcd6d3580d8a5cd5934b066a5ac0fba4
aliakberaakash <ali852609@gmail.com>
2020-10-26 22:59:38 +0600

    added coroutine to networkSource

--- app/src/main/java/com/example/helloworld/data/datasources/networksource/MessageNetworkSourceImpl.kt
+++ app/src/main/java/com/example/helloworld/data/datasources/networksource/MessageNetworkSourceImpl.kt
@@ -2,7 +2,9 @@ package com.example.helloworld.data.datasources.networksource
 
 import com.example.helloworld.data.main.model.Message
 import com.example.helloworld.core.data.network.onResponse
+import com.example.helloworld.core.data.network.responseOrException
 import io.reactivex.Maybe
+import retrofit2.Response
 import javax.inject.Inject
 
 class MessageNetworkSourceImpl @Inject constructor(private val networkService: MessageNetworkService): MessageNetworkSource {
@@ -10,5 +12,10 @@ class MessageNetworkSourceImpl @Inject constructor(private val networkService: M
     override fun getMessage(): Maybe<Message> {
         return networkService.getMessage()
             .onResponse()
+    }
+
+    override suspend fun getCoroutineMessage() : Response<Message> {
+        return networkService.getCoroutineMessage()
+            .responseOrException()
     }
 }
